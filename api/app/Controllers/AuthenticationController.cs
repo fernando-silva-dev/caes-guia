e5d2c56e0081bf1a01f2cea5d60f966ac16eae6b
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-02-14 00:44:01 -0300

    removendo arquivos de build

--- api/app/Controllers/AuthenticationController.cs
+++ api/app/Controllers/AuthenticationController.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace app.Controllers;
 
 [ApiController]
---
+++ api/domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace domain;
+public class User
+{
+    public User(string userName, string password)
+    {
+        UserName = userName;
+        Password = password;
+    }
+
+    public string UserName { get; protected set; }
+    public string Password { get; protected set; }
+}
