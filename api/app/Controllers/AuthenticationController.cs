aa5e8bad7a6f53d2c9c706683b25e47d51c5f330
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-03-03 14:22:29 -0300

    Adicionando classes referentes a usuário e login

--- api/app/Controllers/AuthenticationController.cs
+++ api/app/Controllers/AuthenticationController.cs
@@ -1,11 +1,43 @@
-namespace app.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using App.Models;
+using Service.Services;
+using Service.Models;
+
+namespace App.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("[controller]")]
 public class AuthenticationController : ControllerBase
 {
-    [HttpGet(Name = "Teste")]
-    public string Get(){
+    private readonly TokenGenerator TokenHandler;
+    private readonly UserService Service;
+    public AuthenticationController(TokenGenerator tokenHandler, UserService service)
+    {
+        TokenHandler = tokenHandler;
+        Service = service;
+    }
+
+    [HttpPost]
+    [Route("login")]
+    [AllowAnonymous]
+    public ActionResult<dynamic> Authenticate([FromBody] Credentials model)
+    {
+        UserModel? user = Service.GetUser(model.Username, model.Password);
+        if (user == null)
+            return NotFound(new { message = "Usuário ou senha inválidos" });
+
+        var token = TokenHandler.GenerateToken(user);
+        return new
+        {
+            user = user,
+            token = token
+        };
+    }
+
+    [HttpGet("Teste")]
+    public string Teste()
+    {
         return "hello world!";
     }
 }
--- api/app/Program.cs
+++ api/app/Program.cs
@@ -1,8 +1,35 @@
+using System.Text;
+using App;
+using Crosscutting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+var key = builder.Configuration.GetValue<string>("TOKEN_KEY") ?? throw new ArgumentNullException("TOKEN_KEY");
+var keyBytes = Encoding.ASCII.GetBytes(key);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddScoped<TokenGenerator>();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.InjectDependencies();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,10 +41,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
