27e0b3dd4fef684a8cef054a7fa5d63cc5fc8df8
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-03-03 14:22:51 -0300

    Arquivos que faltaram no commit anterior

---
+++ api/domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities;
+public class User
+{
+    public User(string userName, string password, string role)
+    {
+        Username = userName;
+        Password = password;
+        Role = role;
+    }
+
+    protected User() {}
+
+    public string Username { get; protected set; }
+    public string Password { get; protected set; }
+    public string Role { get; protected set; }
+}
