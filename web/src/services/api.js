a9266f9b3dcad7b43aad984be7ace0fd4d44a4f2
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-03-30 21:25:00 -0300

    Implementando cookies para campo remember me

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,6 +2,11 @@ import axios from "axios";
 // TODO usar variáveis de ambiente
 const api = axios.create({
   baseURL: "http://localhost:5164",
+  timeout: 5000,
+  headers: {
+    'Authorization': "JWT_TOKEN",
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
