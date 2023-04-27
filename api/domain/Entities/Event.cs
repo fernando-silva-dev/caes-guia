1257ac64eb75d75774601c74fd2f49624f3ef089
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-04-27 17:27:47 -0300

    event entity

---
+++ api/domain/Entities/Event.cs
@@ -0,0 +1,20 @@
+namespace Domain.Entities;
+
+public class Event : BaseEntity
+{
+    public Event(string description, DateTime date, Guid dogId)
+    {
+        Id = Guid.NewGuid();
+
+        Description = description;
+        Date = date;
+        DogId = dogId;
+
+        // TODO validate
+    }
+
+    public string Description { get; protected set; }
+    public DateTime Date { get; protected set; }
+    public Guid DogId { get; protected set; }
+    public virtual Dog Dog { get; protected set; }
+}
