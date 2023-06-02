f870ee8366a491978f3deb339a0d39ad260feda8
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-06-02 12:03:22 -0300

    Tasks de geração de reports de qualidade

--- api/test/DomainTests/EntityTests/UserTests.cs
+++ api/test/DomainTests/EntityTests/UserTests.cs
@@ -7,12 +7,14 @@ public class UserTests
     [Fact]
     public void Constructor_Should_Construct()
     {
-        var validConstruction = () => new User("admin", "password", Role.Admin, "name", "12345678901", "1234567890", new Address());
+        var address = new Address("89110000", "Gaspar", "Adriano Kormann", "", "SC", "Bela Vista", "SN");
+        var validConstruction = () => new User("admin", "password", Role.Admin, "name", "12345678901", "1234567890", address);
 
         validConstruction.Should().NotThrow();
         validConstruction().Should().BeOfType<User>();
     }
 
+    // TODO transformar em theory e verificar cada problema de validação
     [Fact]
     public void Constructor_Should_ThrowValidationException()
     {
--- api/test/DomainTests/ValidatorTests/UserValidatorTests.cs
+++ api/test/DomainTests/ValidatorTests/UserValidatorTests.cs
@@ -9,25 +9,28 @@ public class UserValidatorTests
     const string _21CharacterLongString = "Lorem ipsum dolor sit";
     const string _200CharacterLongString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies volutpat nisi eget ornare. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin al";
     const string _201CharacterLongString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies volutpat nisi eget ornare. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin ali";
+    static Address validAddress = new Address("89110000", "Gaspar", "Adriano Kormann", "", "SC", "Bela Vista", "SN");
     #endregion
 
-    // [Theory]
-    // [InlineData("a", "password", true)]
-    // [InlineData(_20CharacterLongString, _200CharacterLongString, true)]
-    // [InlineData("", "password", false)]
-    // [InlineData("admin", "", false)]
-    // [InlineData(_21CharacterLongString, "password", false)]
-    // [InlineData("admin", _201CharacterLongString, false)]
-    // public void Validation_Should_HaveExpectedResult(string username, string password, Role role, string name, string cpf, string phone, bool throwExpected)
-    // {
-    //     User user = new User(username, password, role, name, cpf, phone, null);
-    //     UserValidator validator = new();
+    [Theory]
+    [InlineData("a", "password", Role.Operador, "Felipe", "99999999999", "4799999999", true)]
+    [InlineData(_20CharacterLongString, _200CharacterLongString, Role.Operador, "Felipe", "99999999999", "4799999999", true)]
+    [InlineData("", "password", Role.Operador, "Felipe", "99999999999", "4799999999", false)]
+    [InlineData("admin", "", Role.Operador, "Felipe", "99999999999", "4799999999", false)]
+    [InlineData(_21CharacterLongString, "password", Role.Operador, "Felipe", "99999999999", "4799999999", false)]
+    [InlineData("admin", _201CharacterLongString, Role.Operador, "Felipe", "99999999999", "4799999999", false)]
+    // TODO adicionar mais casos 
+    public void Validation_Should_HaveExpectedResult(string username, string password, Role role, string name, string cpf, string phone, bool throwExpected)
+    {
+        User user = new User(username, password, role, name, cpf, phone, validAddress);
+        UserValidator validator = new();
 
-    //     var validation = () => validator.ValidateAndThrow(user);
+        var validation = () => validator.ValidateAndThrow(user);
 
-    //     if (throwExpected)
-    //         validation.Should().NotThrow();
-    //     else
-    //         validation.Should().Throw<ValidationException>();
-    // }
+        if (throwExpected)
+            validation.Should().NotThrow();
+        else
+            validation.Should().Throw<ValidationException>();
+    }
+    // TODO fazer método que teste validação de endereço
 }
