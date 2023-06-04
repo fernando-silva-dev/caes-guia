d6cf4f26aae263ccb3e5a7ee8bd1e1865b3f7abb
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-06-04 14:41:34 -0300

    Migrations, configuração de coverlet

---
+++ api/repository/Migrations/20230604173450_Seed.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Users", new String[] 
+            { "Username", "Password", "Role", "Id", "Name" }, new Object[] 
+            { "Admin", "Admin", 1, Guid.NewGuid(), "Administrador" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
