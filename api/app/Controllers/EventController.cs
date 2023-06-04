037818504a5f69246b5e5fd4579d67eff50d51d4
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-06-04 15:14:13 -0300

    Endpoint de busca de evento por cão

--- api/app/Controllers/EventController.cs
+++ api/app/Controllers/EventController.cs
@@ -1,3 +1,4 @@
+using App.Models;
 using Service.Models.Event;
 
 namespace App.Controllers;
@@ -6,5 +7,15 @@ public class EventController : BaseCrudController<EventInsertModel, EventViewMod
 {
     public EventController(IEventService service) : base(service)
     {
+    }
+
+    [HttpGet("dog/{dogId}")]
+    public ActionResult<PagedResult<EventViewModel>> List([FromRoute] Guid dogId, [FromQuery] int page = 0, [FromQuery] int size = 10)
+    {
+        var query = Service.List().Where(x => x.DogId == dogId);
+        var data = query.Take(size).Skip(page * size);
+        var totalRecords = query.Count();
+
+        return Ok(new PagedResult<EventViewModel> { Data = data, TotalRecords = totalRecords });
     }
 }
