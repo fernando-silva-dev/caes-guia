38d9007e5943e0ffb8119bbefa70d8ff540e3c27
Fernando Santos da Silva <fernando.dasilva@ambevtech.com.br>
2023-10-19 10:58:32 -0300

    Sending attachments on events

--- api/repository/repositories/EventRepository.cs
+++ api/repository/repositories/EventRepository.cs
@@ -7,4 +7,9 @@ public class EventRepository : BaseRepository<Event>, IEventRepository
     public EventRepository(Context context) : base(context)
     {
     }
+
+    public override IQueryable<Event> List()
+    {
+        return Context.Events.Include(x => x.Attachments);
+    }
 }
