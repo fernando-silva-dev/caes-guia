c11e25e60df886a989ef75fd6c44d2c2bcc22526
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-11-05 23:11:39 -0300

    migration

---
+++ api/repository/Migrations/20231021030107_DogCoat.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class DogCoat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Dogs");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Coat",
+                table: "Dogs",
+                type: "integer",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Coat",
+                table: "Dogs");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Dogs",
+                type: "character varying(25)",
+                maxLength: 25,
+                nullable: true);
+        }
+    }
+}
