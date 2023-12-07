18ae942d4f20ce824cf0bf3f4e02db4fb6cea119
fernando-silva-dev <fernando-silva-dev@gmail.com>
2023-12-07 16:16:56 -0300

    WIP: instalador

---
+++ installer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+Process cmd = new Process();
+cmd.StartInfo.FileName = "cmd.exe";
+cmd.StartInfo.RedirectStandardInput = true;
+cmd.StartInfo.RedirectStandardOutput = true;
+cmd.StartInfo.CreateNoWindow = true;
+cmd.StartInfo.UseShellExecute = false;
+cmd.Start();
+
+// docker-compose up e tals
+cmd.StandardInput.WriteLine("");
+cmd.StandardInput.Flush();
+cmd.StandardInput.Close();
+cmd.WaitForExit();
+Console.WriteLine(cmd.StandardOutput.ReadToEnd());
